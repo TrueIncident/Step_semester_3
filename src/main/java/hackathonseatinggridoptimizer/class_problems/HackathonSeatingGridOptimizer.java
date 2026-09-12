ac283a00af9754d461059e28dd4bd5cbea6bf089
TrueIncident <argha736@gmail.com>
2026-09-12 11:39:38 +0530

    Add Week 5 classwork Problem 4

---
+++ src/main/java/hackathonseatinggridoptimizer/class_problems/HackathonSeatingGridOptimizer.java
@@ -0,0 +1,38 @@
+package hackathonseatinggridoptimizer.class_problems;
+
+public class HackathonSeatingGridOptimizer {
+    private static double rowAverage(int[] row) {
+        int sum = 0;
+        for (int score : row) {
+            sum += score;
+        }
+        return (double) sum / row.length;
+    }
+
+    static String classifyRows(int[][] seatingScores, int threshold) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < seatingScores.length; i++) {
+            double average = rowAverage(seatingScores[i]);
+
+            if (i > 0) {
+                result.append(" | ");
+            }
+
+            result.append("Row ").append(i).append(": ");
+            result.append(average < threshold ? "Quiet Zone" : "Buzzing Zone");
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        int[][] seatingScores = {
+            {40, 50, 45},
+            {85, 90, 95},
+            {30, 20, 25}
+        };
+        int threshold = 60;
+        System.out.println(classifyRows(seatingScores, threshold));
+    }
+}
