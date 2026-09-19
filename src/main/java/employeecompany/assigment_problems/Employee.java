668aacdd6842a8925518b648b06081dac68823af
TrueIncident <argha736@gmail.com>
2026-09-19 11:17:13 +0530

    Move Week 6 HW Category C problem to Session 5

---
+++ src/main/java/employeecompany/assigment_problems/Employee.java
@@ -0,0 +1,27 @@
+package employeecompany.assigment_problems;
+
+public class Employee {
+    String empName;
+    double salary;
+    static String companyName = "Bright Horizon Technologies";
+    static int employeeCount = 0;
+
+    public Employee(String empName, double salary) {
+        this.empName = empName;
+        this.salary = salary;
+        employeeCount++;
+    }
+
+    static void printCompanyInfo() {
+        System.out.println(companyName);
+        System.out.println("Employees on record: " + employeeCount);
+    }
+
+    public static void main(String[] args) {
+        new Employee("Divya", 65000);
+        new Employee("Arjun", 0);
+        new Employee("Ravi", 55000);
+
+        Employee.printCompanyInfo();
+    }
+}
